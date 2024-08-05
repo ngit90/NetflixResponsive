45dc389afec3d0b4155b4cfe49f9f35002848057
Noopura das P P <noopu.das90@gmail.com>
2024-08-05 21:56:20 +0530

    netflix aug 05 last

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function myfunction(){
 
-    document.getElementById("lstbtn").style.display="grid";
-    document.getElementById("lstbtn").style.listStyle="none";
-    document.getElementById("lstbtn").style.color="#ffff";
-}
 $(document).ready(function(){
     $("#form11").validate({
         rules:{
